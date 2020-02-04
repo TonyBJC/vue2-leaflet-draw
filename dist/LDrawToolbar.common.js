6c6803d5cec86ff25d0a40196f8caf7d8cb7dac3
Solarman <solarman@Solarmans-MacBook-Pro.local>
2020-02-04 11:51:12 +0800

    first

--- dist/LDrawToolbar.common.js
+++ dist/LDrawToolbar.common.js
@@ -1094,12 +1094,12 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"2d88d939-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/App.vue?vue&type=template&id=64e7103e&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"2d88d939-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/App.vue?vue&type=template&id=4fae0dfe&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticStyle:{"display":"none"}})}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/App.vue?vue&type=template&id=64e7103e&
+// CONCATENATED MODULE: ./src/App.vue?vue&type=template&id=4fae0dfe&
 
 // EXTERNAL MODULE: ./node_modules/leaflet-draw/dist/leaflet.draw.js
 var leaflet_draw = __webpack_require__("20d6");
@@ -1235,7 +1235,7 @@ var actions_Delete = __webpack_require__("a9b7");
     var _this = this;
 
     this.$nextTick(function () {
-      var map = _this.$parent.$parent.$parent.$parent.$parent.$refs.map.mapObject;
+      var map = _this.$parent.$parent.$parent.$parent.$refs.map.mapObject;
       var editActions = [L.Toolbar2.EditAction.Popup.Edit, L.Toolbar2.EditAction.Popup.Delete, L.Toolbar2.Action.extendOptions({
         toolbarIcon: {
           className: 'leaflet-color-picker',
