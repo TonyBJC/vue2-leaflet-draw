f10efa91d8cf84db186e64aa6776e33a50360f3a
张冬冬 <yidongkaifa@126.com>
2020-03-08 22:27:20 +0800

    加 事件

--- dist/LDrawToolbar.common.js
+++ dist/LDrawToolbar.common.js
@@ -1096,12 +1096,12 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"2d88d939-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/App.vue?vue&type=template&id=6e0f5392&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"2d88d939-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/App.vue?vue&type=template&id=8f93e02c&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticStyle:{"display":"none"}})}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/App.vue?vue&type=template&id=6e0f5392&
+// CONCATENATED MODULE: ./src/App.vue?vue&type=template&id=8f93e02c&
 
 // EXTERNAL MODULE: ./node_modules/leaflet-draw/dist/leaflet.draw.js
 var leaflet_draw = __webpack_require__("20d6");
@@ -1274,7 +1274,7 @@ var actions_Delete = __webpack_require__("a9b7");
       map.on('draw:edited', function (e) {
         // var type = e.layerType,
         //      const   e = e;
-        that.$emit('edited', e);
+        that.$emit('edited', e.layers);
       });
     });
   }
